55ee82b0e43973f4f8e58bbf616e79b899621eaa
mnin-nahid <nahidulislam.nayan54@gmail.com>
2023-10-11 18:42:33 +0600

    infix to postfix

--- infix-to-postfix-using-stack.c
+++ infix-to-postfix-using-stack.c
@@ -1,12 +1,9 @@
-
-// C code to convert infix to postfix expression 
-  
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
 #include<ctype.h>
   
-#define MAX_EXPR_SIZE 100 
+#define MAX 100 
   
 // Function to return precedence of operators 
 int precedence(char operator) 
@@ -15,46 +12,45 @@ int precedence(char operator)
     case '+': 
     case '-': 
         return 1; 
+        break;
     case '*': 
     case '/': 
         return 2; 
+        break;
     case '^': 
         return 3; 
+        break;
     default: 
         return -1; 
     } 
 } 
   
-// Function to check if the scanned character  
-// is an operator 
+// Function to check if the scanned character is an operator 
 int isOperator(char ch) 
 { 
     return (ch == '+' || ch == '-' || ch == '*' || ch == '/'
-            || ch == '^'); 
+            || ch == '^' || ch == '('|| ch == ')'); 
 } 
   
-// Main functio to convert infix expression 
-// to postfix expression 
+// Main functio to convert infix expression to postfix expression 
 char* infixToPostfix(char* infix) 
 { 
     int i, j; 
     int len = strlen(infix); 
     char* postfix = (char*)malloc(sizeof(char) * (len + 2)); 
-    char stack[MAX_EXPR_SIZE]; 
+    char stack[MAX]; 
     int top = -1; 
   
     for (i = 0, j = 0; i < len; i++) { 
         if (infix[i] == ' ' || infix[i] == '\t') 
             continue; 
         
-        // If the scanned character is operand 
-        // add it to the postfix expression 
+        // If the scanned character is operand add it to the postfix expression 
         if (isalnum(infix[i])) { 
             postfix[j++] = infix[i]; 
         } 
         
-        // if the scanned character is '(' 
-        // push it in the stack 
+        // if the scanned character is '(' push it in the stack 
         else if (infix[i] == '(') { 
             stack[++top] = infix[i]; 
         } 
@@ -96,7 +92,9 @@ char* infixToPostfix(char* infix)
 // Driver code 
 int main() 
 { 
-    char infix[MAX_EXPR_SIZE] = "a+(b*c-(d/e^f)*g)*h"; 
+    char infix[MAX];
+    printf("Enter the String : ");
+    fgets(infix, sizeof(infix), stdin);  
     
     // Function call 
     char* postfix = infixToPostfix(infix); 
