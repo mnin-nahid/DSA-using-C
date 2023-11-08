43a003ec0799feae5d8238b527e5375b424b8fcd
mnin-nahid <nahidulislam.nayan54@gmail.com>
2023-11-08 20:23:13 +0600

    quick sort with last element for pivot

---
+++ dac/quick-sort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void swap(int *x, int *y);
+void quicksort(int array[], int length);
+void quicksort_recursion(int array[], int low, int high);
+int partition(int array[], int low, int high);
+int main()
+{
+    int array[] = {4, 9, 2, 1, 8, 5, 96, 34, 91};
+    int length = sizeof(array) / sizeof(array[1]);
+
+    quicksort(array, length);
+
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d, ", array[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void quicksort(int array[], int length)
+{
+    quicksort_recursion(array, 0, length - 1);
+}
+
+void quicksort_recursion(int array[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot_index = partition(array, low, high);
+        quicksort_recursion(array, low, pivot_index - 1);
+        quicksort_recursion(array, pivot_index + 1, high);
+    }
+}
+
+int partition(int array[], int low, int high){
+    int pivot_value = array[high];
+    int i = low;
+    for(int j = low; j< high; j++){
+        if(array[j] <= pivot_value){
+            swap(&array[i], &array[j]);
+            i++;
+        }
+    }
+    swap(&array[i], &array[high]);
+
+    return i;
+}
